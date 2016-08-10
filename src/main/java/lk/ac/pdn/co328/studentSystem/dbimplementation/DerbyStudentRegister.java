53452518cdb00044fea5ee218a771177303fc6ad
Dinithi Weerasekera <e12369@ce.pdn.ac.lk>
2016-08-10 23:04:15 +0530

    derby database handling

--- src/main/java/lk/ac/pdn/co328/studentSystem/dbimplementation/DerbyStudentRegister.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/dbimplementation/DerbyStudentRegister.java
@@ -5,11 +5,9 @@
  */
 package lk.ac.pdn.co328.studentSystem.dbimplementation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.lang.*;
 import lk.ac.pdn.co328.studentSystem.Student;
 import lk.ac.pdn.co328.studentSystem.StudentRegister;
 
@@ -20,37 +18,45 @@ import lk.ac.pdn.co328.studentSystem.StudentRegister;
 public class DerbyStudentRegister extends StudentRegister {
 
     Connection connection = null;
-    public DerbyStudentRegister() throws SQLException
+    public DerbyStudentRegister()
     {
-            String dbURL1 = "jdbc:derby:codejava/studentDB;create=true";
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        }
+        catch(java.lang.ClassNotFoundException e) {
+            System.err.print("ClassNotFoundException:");
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            String dbURL1 = "jdbc:derby:\\db\\studentDB.db;create=true";
             connection = DriverManager.getConnection(dbURL1);
-            if (connection != null)
-            {
-                String SQL_CreateTable = "CREATE TABLE Students(id INT , name VARCHAR(24))";
-                System.out.println ( "Creating table addresses..." );
-                try 
-                {
+            if (connection != null) {
+                String SQL_CreateTable = "CREATE TABLE Register(id INT , name VARCHAR(24) , lname VARCHAR(24))";
+                System.out.println("Creating table addresses...");
+                try {
                     Statement stmnt = connection.createStatement();
-                    stmnt.execute( SQL_CreateTable );
+                    stmnt.execute(SQL_CreateTable);
                     stmnt.close();
                     System.out.println("Table created");
-                } catch ( SQLException e )
-                {
+                } catch (SQLException e) {
                     System.out.println(e);
                 }
-               System.out.println("Connected to database");
-            }
-            else
-            {
+                System.out.println("Connected to database");
+            } else {
                 throw new SQLException("Connection Failed");
             }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
     
     @Override
     public void addStudent(Student st) throws Exception {
         if (connection != null)
         {
-            String SQL_AddStudent = "INSERT INTO Students VALUES (" + st.getId() + ",'" + st.getFirstName() + "')";
+            String SQL_AddStudent = "INSERT INTO Register VALUES (" + st.getId() + ",'" + st.getFirstName() +"','" + st.getLastName() + "')";
             System.out.println ( "Adding the student..." + SQL_AddStudent);
 
             Statement stmnt = connection.createStatement();
@@ -66,28 +72,108 @@ public class DerbyStudentRegister extends StudentRegister {
     }
 
     @Override
-    public void removeStudent(int regNo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void removeStudent(int regNo) throws Exception{
+        //To change body of generated methods, choose Tools | Templates.
+        if (connection != null) {
+            String SQL_DelStudent = "DELETE FROM Register WHERE id = " + regNo;
+            System.out.println ( "Deleting the student" + SQL_DelStudent);
+            Statement stm = connection.createStatement();
+            stm.execute(SQL_DelStudent);
+            stm.close();
+            System.out.println("Student Deleted");
+        }
+        else
+        {
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public Student findStudent(int regNo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Student findStudent(int regNo) throws Exception{
+        //To change body of generated methods, choose Tools | Templates.
+        if (connection != null) {
+            String SQL_FindStudent = "SELECT * FROM Register WHERE id = " + regNo;
+            System.out.println ( "Finding the student..." + SQL_FindStudent);
+            Statement stm = connection.createStatement();
+            ResultSet students = stm.executeQuery(SQL_FindStudent);
+            System.out.println("Student Found");
+            Student st = null;
+
+            if (students.next()) {
+                st = new Student(students.getInt(1), students.getString(2), students.getString(3));
+            }
+
+            System.out.println("Parsing OK");
+            stm.close();
+            return st;
+        }
+        else {
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public void reset() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void reset() throws Exception{
+        //To change body of generated methods, choose Tools | Templates.
+        if (connection != null) {
+            String SQL_DelStudent = "DELETE FROM Register";
+            System.out.println ( "Deleting all students..." + SQL_DelStudent);
+            Statement stm = connection.createStatement();
+            stm.execute(SQL_DelStudent);
+            stm.close();
+            System.out.println("All Students Deleted");
+        }
+        else {
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public ArrayList<Student> findStudentsByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Student> findStudentsByName(String name) throws Exception{
+        //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Student> students = new ArrayList<Student>();
+        if (connection != null){
+            String SQL_FindStudent = "SELECT * FROM Register WHERE fname = " + name;
+            Statement stm = connection.createStatement();
+            ResultSet studentSet = stm.executeQuery(SQL_FindStudent);
+            while(studentSet.next()) {
+                students.add(new Student(studentSet.getInt(1), studentSet.getString(2), studentSet.getString(3)));
+            }
+            stm.close();
+            SQL_FindStudent = "SELECT * FROM Register WHERE lname = " + name;
+            stm = connection.createStatement();
+            studentSet = stm.executeQuery(SQL_FindStudent);
+            System.out.println("Students Found");
+            while(studentSet.next()) {
+                students.add(new Student(studentSet.getInt(1), studentSet.getString(2), studentSet.getString(3)));
+            }
+            stm.close();
+        }
+        else {
+            throw new Exception("Database Connection Error");
+        }
+
+        return students;
     }
 
     @Override
-    public ArrayList<Integer> getAllRegistrationNumbers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Integer> getAllRegistrationNumbers() throws Exception {
+        //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> students = new ArrayList<Integer>();
+        if (connection != null){
+            String SQL_FindStudent = "SELECT id FROM Register";
+            Statement stm = connection.createStatement();
+            ResultSet studentSet = stm.executeQuery(SQL_FindStudent);
+            while(studentSet.next()) {
+                students.add(studentSet.getInt(1));
+            }
+
+            stm.close();
+        }
+        else {
+            throw new Exception("Database Connection Error");
+        }
+        return students;
     }
     
 }
